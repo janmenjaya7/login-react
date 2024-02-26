0324b32e86ea94f64ba8e6a9da6f6855b2e09cc7
janmenjaya Biswal <142783463+janmenjaya7@users.noreply.github.com>
2024-02-26 19:06:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="iner-card">
+          <form>
+            <input type="text" />
+            <input type="password" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
